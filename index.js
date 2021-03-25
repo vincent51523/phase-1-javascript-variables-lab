0504f28a4bc23ddad992e146ad8d42acc6668156
bin <vincent418@icloud.com>
2021-03-24 22:16:05 -0500

    commit tree dc755eadd410d73291f73d42467bd645e5a8a567

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+//let companyName = 'Scuber';
+const companyName = 'Scuber';
+mostProfitableNeighborhood = 'Chelsea';
+let mostProfitableNeighborhood = 'Chelsea';
+companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
